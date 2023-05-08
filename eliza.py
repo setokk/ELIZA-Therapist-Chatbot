b2952cdecd1e7ef7ad776b92c0819397a2ed64c6
setokk <tesmtracks@gmail.com>
2023-05-08 17:06:33 +0300

    Added flexibility to hey message

--- eliza.py
+++ eliza.py
@@ -15,29 +15,41 @@ if ("windows" in platform.system().lower()):
 WIDTH_SPACE = "       "
 
 # Constants
-EXIT = "exit"
+EXIT = "bye"
+REOPENED = "1"
 
 # Define the types of sentences ELIZA can understand
+hello = re.compile(r"^(Hell(o)+|H(i)+|He(y)+).*$", re.IGNORECASE)
+helloTemplate = ""
+with open("history.eliza", "a+") as f:
+	f.seek(0) # we position a+ stream at the start of file
+	content = f.readline()
+	helloTemplate = "Hello! Good to see you again."
+	if (content != REOPENED):
+		helloTemplate = "Hello!"
+		f.write(REOPENED)
+	
 depressedSad = re.compile(r"(I'm |im |I am |feel )(depressed|sad)", re.IGNORECASE)
 depressedSadTemplate = "I'm sorry you feel {}. Is there anything you want to tell me?"
 
-unhappy = re.compile(r"(I'm |im |I am |feel )(unhappy)", re.IGNORECASE)
+unhappy = re.compile(r"(I'm |im |I am |feel )(still |so |really )?(unhappy)", re.IGNORECASE)
 unhappyTemplate = "Do you think coming here will help you not to be {}?"
 
-unsure = re.compile(r"(((I do not|I don't|I dont) (think|believe|feel certain|feel sure|feel confident))|(I am not sure|I am unsure|I'm not sure|I'm unsure) (that)? I (will(be able to)?|can|am able to)) (endure|go through|get through|solve)", re.IGNORECASE)
+unsure = re.compile(r"(((I do not|I don't|I dont|I) (think|believe|feel certain|feel sure|feel confident|feel like))|(I am not sure|I am unsure|I'm not sure|I'm unsure))( that)? I (will(( not)? be able to)?|can|am able to) (endure|go through|get through|solve)", re.IGNORECASE)
 unsureTemplate = "I am sure you will {} your problems.\n" + WIDTH_SPACE + "It is okay to feel this way when you are stressed or if you recently had a loss.\n" + WIDTH_SPACE + "Does taking a walk and relaxing sound like a good idea?"
 
 problems = re.compile(r"(I (have|encountered) (a |A |an ))(problem|issue)", re.IGNORECASE)
 problemsTemplate = "Can you tell me more about the {}?"
 
-yes = re.compile(r"yes", re.IGNORECASE)
+yes = re.compile(r"^(Yes)$", re.IGNORECASE)
 yesTemplate = "I'm glad we agree!"
 
 # List of regex and responses (third parameter indicates which groups should replace the '{}')
 responseGenerators = []
+responseGenerators.append(ResponseGenerator(helloTemplate, hello, []))
 responseGenerators.append(ResponseGenerator(depressedSadTemplate, depressedSad, [2]))
-responseGenerators.append(ResponseGenerator(unhappyTemplate, unhappy, [2]))
-responseGenerators.append(ResponseGenerator(unsureTemplate, unsure, [9]))
+responseGenerators.append(ResponseGenerator(unhappyTemplate, unhappy, [3]))
+responseGenerators.append(ResponseGenerator(unsureTemplate, unsure, [10]))
 responseGenerators.append(ResponseGenerator(problemsTemplate, problems, [4]))
 responseGenerators.append(ResponseGenerator(yesTemplate, yes, []))
 
@@ -53,7 +65,7 @@ print(f"""
 print(f"A simple chatbot using {BLUE}regex{END_COLOR} for {BLUE}Theory of Calculation{END_COLOR} course, University of Macedonia.\n\n\n\n")
 
 
-message = str(input("You: "))
+message = str(input(f"{BLUE}You:{END_COLOR}   "))
 while (message.lower() != EXIT):
     isMatched = False
     for generator in responseGenerators:
@@ -65,6 +77,6 @@ while (message.lower() != EXIT):
     if (not isMatched):
         print(f"{RED}ELIZA:{END_COLOR} I don't think I understood that correctly...")
     
-    message = str(input("You: "))
+    message = str(input(f"{BLUE}You:{END_COLOR}   "))
 
 print(f"{GREEN}ELIZA:{END_COLOR} Have a nice day. Bye!")
